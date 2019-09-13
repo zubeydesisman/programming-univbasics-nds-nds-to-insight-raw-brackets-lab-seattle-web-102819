f30a57b47eb3491109a19c2679c86bca646e1264
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-13 16:28:37 -0400

    base tests

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,11 +1,8 @@
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
-def pretty_print_nds
+def directors_totals
   nds = directors_database
   # Change the code below to pretty print the nds with pp
   nil
-end
-
-def print_first_directores_movie_title
 end
