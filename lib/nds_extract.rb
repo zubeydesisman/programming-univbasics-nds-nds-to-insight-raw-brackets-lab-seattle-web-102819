7c62cb8b8657d06e908643e6fe380c685e6ac869
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-15 06:29:07 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,19 +10,31 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
-  i = 0 
-  while i< nds.length do 
-    key = nds[i][:name] 
+#   i = 0 
+#   while i< nds.length do 
+#     key = nds[i][:name] 
     
-    j = 0 
-    movie_data = nds[i][:movies]
-    value = 0
-    while j < movie_data.length do
-      value += movie_data[j][:worldwide_gross]
-      j += 1
+#     j = 0 
+#     movie_data = nds[i][:movies]
+#     value = 0
+#     while j < movie_data.length do
+#       value += movie_data[j][:worldwide_gross]
+#       j += 1
+#     end
+#     i += 1
+#     result[key] = value
+#   end
+#   return result
+# end
+  for name_movies in nds do
+   value = 0
+    key = name_movies[:name]
+    movie_array = name_movies[:movies]
+    for movie in movie_array do
+     value += movie[:worldwide_gross]
     end
-    i += 1
     result[key] = value
   end
   return result
 end
+  
