26ef862c63544312829503123ef9ed20bf390af8
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-15 06:09:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,19 +10,19 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
-  i = 0 
-  while i< nds.length do 
-    key = nds[i][:name] 
+#   i = 0 
+#   while i< nds.length do 
+#     key = nds[i][:name] 
     
-    j = 0 
-    movie_data = nds[i][:movies]
-    value = 0
-    while j < movie_data.length do
-      value += movie_data[j][:worldwide_gross]
-      j += 1
-    end
-    i += 1
-    result[key] = value
-  end
-  return result
-end
+#     j = 0 
+#     movie_data = nds[i][:movies]
+#     value = 0
+#     while j < movie_data.length do
+#       value += movie_data[j][:worldwide_gross]
+#       j += 1
+#     end
+#     i += 1
+#     result[key] = value
+#   end
+#   return result
+# end
